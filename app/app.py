3e492132b8f8fadb755a4e7cc0fc51fe7bd150fb
mahalakshmi <alikepallimahalakshmi @gmail.com>
2024-03-19 22:53:27 +0530

    Initial commit

---
+++ app/app.py
@@ -0,0 +1,64 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+import cv2
+import mediapipe as mp
+import pyautogui
+
+cap = cv2.VideoCapture(0)
+
+hand_detector = mp.solutions.hands.Hands()
+drawing_utils = mp.solutions.drawing_utils
+screen_width, screen_height = pyautogui.size()
+index_x, index_y = 0, 0
+thumb_x, thumb_y = 0, 0
+
+while True:
+    ret, frame = cap.read()
+    frame = cv2.flip(frame, 1)
+    frame_height, frame_width, _ = frame.shape
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    output = hand_detector.process(rgb_frame)
+    hands = output.multi_hand_landmarks
+
+    if hands:
+        for hand in hands:
+            drawing_utils.draw_landmarks(frame, hand, mp.solutions.hands.HAND_CONNECTIONS)
+            landmarks = hand.landmark
+            for id, landmark in enumerate(landmarks):
+                x = int(landmark.x * frame_width)
+                y = int(landmark.y * frame_height)
+                if id == 8:  # Index finger
+                    index_x = screen_width * landmark.x
+                    index_y = screen_height * landmark.y
+                elif id == 4:  # Thumb
+                    thumb_x = screen_width * landmark.x
+                    thumb_y = screen_height * landmark.y
+                    cv2.line(frame, (int(index_x), int(index_y)), (int(thumb_x), int(thumb_y)), (0, 255, 0),
+                             2)  # Green line between index and thumb
+
+                    if abs(index_y - thumb_y) < 20:
+                        pyautogui.click()
+                        pyautogui.sleep(0.5)
+                    elif abs(index_y - thumb_y) < 100:
+                        pyautogui.moveTo(index_x, index_y, duration=0.2)  # Slower cursor movement
+
+    cv2.imshow('Virtual Mouse', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
